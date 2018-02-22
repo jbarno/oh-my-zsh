141fe59812bf166ff97fcabdb7a42e75b23ef4c7
Justin Barnowski <jbarno@uchicago.edu>
2018-02-22 14:13:12 -0600

    better git log, todo add color

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -177,7 +177,7 @@ alias glgm='git log --graph --max-count=10'
 alias glo='git log --oneline --decorate'
 alias glol="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias glola="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
-alias glog='git log --oneline --decorate --graph'
+alias glog="vim -R -c 'set filetype=git nowrap' <(git log --graph --pretty=format:'%h – %d %s (%cr) <%an> ')"
 alias gloga='git log --oneline --decorate --graph --all'
 alias glp="_git_log_prettily"
 compdef _git glp=git-log
